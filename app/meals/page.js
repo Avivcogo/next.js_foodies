6d5ebac918471dbc6ffe8e7ea0f09600eefe8f58
Avivcogo <avivcogo@gmail.com>
2025-05-26 14:01:28 +0200

    wip

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,28 @@
-import Link from 'next/link';
-export default function MealsPage() {
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+
+export default async function MealsPage() {
+  const meals = await getMeals();
+
   return (
-    <main>
-      <h1>MEALS</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. it is easy and fun
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipie</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals}/>
+      </main>
+    </>
   );
 }
