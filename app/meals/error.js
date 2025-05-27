f3492d78300cf1d0a82a0e4deab77e5127ffa336
Avivcogo <avivcogo@gmail.com>
2025-05-27 13:38:21 +0200

    wip

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function ErrorPage({error}) {
+    return(
+        <main className="error">
+            <h1>An Error Occurred!</h1>
+            <p>Failed to fetch meal data. please try again later.</p>
+        </main>
+    )
+}
