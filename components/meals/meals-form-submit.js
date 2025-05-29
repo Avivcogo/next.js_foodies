9babf60166fcd44620f93ed0faece8a469430e36
Avivcogo <avivcogo@gmail.com>
2025-05-29 12:48:45 +0200

    useFormStatus

--- components/meals/meals-form-submit.js
+++ components/meals/meals-form-submit.js
@@ -1,12 +1,12 @@
-// "use client";
+"use client";
 
-// import { useFormStatus } from "react-dom";
+import { useFormStatus } from "react-dom";
 
-// export default function MealsFormSubmit() {
-//   const { pending } = useFormStatus();
-//   return (
-//     <button disabled={pending}>
-//         {pending ? "Submitting..." : "Share Meal"}
-//         </button>
-//   );
-// }
+export default function MealsFormSubmit() {
+  const { pending } = useFormStatus();
+  return (
+    <button disabled={pending}>
+        {pending ? "Submitting..." : "Share Meal"}
+        </button>
+  );
+}
